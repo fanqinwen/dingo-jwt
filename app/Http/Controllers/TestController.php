8756cc1228d8db3319b136ea1a377236fcb54327
lixiaowei <lixiaowei@mioji.com>
2018-06-15 14:59:03 +0800

    test route

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lixiaowei
+ * Date: 2018/6/15
+ * Time: 下午2:55
+ */
+
+namespace App\Http\Controllers;
+
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        echo 'Hello Word!';
+    }
+}
