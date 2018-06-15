d67b34d16dd446ddff13239966b1205b125e267e
lixiaowei <lixiaowei@mioji.com>
2018-06-15 17:17:14 +0800

    jwt+dingo 集成

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-//发送验证码
+//测试token
 Route::get('/test', 'TestController@index');
